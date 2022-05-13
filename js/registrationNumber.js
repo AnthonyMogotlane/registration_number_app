a461182de114439ab82ecc82d36705b150fc8500
Anthony <almogotlane@gmail.com>
2022-05-13 16:54:28 +0200

    update registrationNumber.js logic

--- js/registrationNumber.js
+++ js/registrationNumber.js
@@ -1,5 +1,6 @@
 const registrationNumber = () => {
     let regNum;
+    let townName;
     let validatedInput;
     let listOfRegNum = [];
     //set the input value
@@ -16,17 +17,17 @@ const registrationNumber = () => {
     const getConvert = () => setConvert();
     // validate the input value
     const setValidation = () => {
-        if(setConvert() === "") return "Enter a registration number";
-        if(setConvert().length > 10) return "Registration number should be less that 10 charactors";
-        if(!/CF|CL|CA|CK/.test(setConvert())) return "Only CF, CL, CA, CK Town indicators allowed";
-        if(/[~!@#$%^&*()+_"?><:]/.test(setConvert())) return "Invalid charator input";
+        if (setConvert() === "") return "Enter a registration number";
+        if (setConvert().length > 10) return "Registration number should be less that 10 charactors";
+        if (!/CF|CL|CA|CK/.test(setConvert())) return "Only CF, CL, CA, CK Town indicators allowed";
+        if (/[~!@#$%^&*()+_"?><:]/.test(setConvert())) return "Invalid charator input";
         return validatedInput = setConvert();
     }
     const getValidation = () => setValidation();
     //set a list of registration numbers
     const setListOfRegNum = item => {
-        if(validatedInput === setConvert()) {
-            if(item !== undefined) {
+        if (validatedInput === setConvert()) {
+            if (item !== undefined) {
                 listOfRegNum.push(item);
             }
         }
@@ -34,6 +35,26 @@ const registrationNumber = () => {
     }
     //get the list registration numbers added
     const getListOfRegNum = () => setListOfRegNum();
+    //set the name of the town to filter
+    const setTown = selectTown => townName = selectTown;
+    //get the town
+    const getTown = () => townName  ;
+    //filter list with registration number indicator
+    let towns = {
+        "malmesbury": "CK",
+        "stellenbosch": "CL",
+        "cape town": "CA",
+        "kuilsriver": "CF",
+    }
+    const setFilterList = () => {
+        for(let town in towns) {
+            if(town === getTown()) {
+                return (getListOfRegNum().filter(num => num.includes(towns[town])))
+            }
+        }
+    }
+    //get the filter list
+    const getFilterList = () => setFilterList();
 
     return {
         setInput,
@@ -43,17 +64,25 @@ const registrationNumber = () => {
         setValidation,
         getValidation,
         setListOfRegNum,
-        getListOfRegNum
+        getListOfRegNum,
+        setTown,
+        getTown,
+        setFilterList,
+        getFilterList
     }
 }
 
-let test = registrationNumber();
+let registration = registrationNumber();
+registration.setInput("CF 654-953");
+registration.setListOfRegNum(registration.setValidation())
+registration.setInput("CA 654-953");
+registration.setListOfRegNum(registration.setValidation())
+registration.setInput("CL 654-953");
+registration.setListOfRegNum(registration.setValidation())
+registration.setInput("CK 654-953");
+registration.setListOfRegNum(registration.setValidation())
 
-test.setInput("ca 365-951");
-test.setListOfRegNum(test.getValidation())
+registration.setTown("kuilsriver");
 
-test.setInput("cK 365-548");
-test.setListOfRegNum(test.getValidation())
-
-
-console.log(test.getListOfRegNum());
+console.log(registration.getTown());
+console.log(registration.setFilterList());
