6685193f250859c097a766375d9233b4b1cc46e9
Anthony Mogotlane <almogotlane@gmail.com>
2022-05-14 23:27:02 +0200

    add logic to registration_number.js

--- js/registration_number.js
+++ js/registration_number.js
@@ -9,45 +9,11 @@ const headerContent = document.querySelector(".header-content");
 
 //instance of registrationNumber factory function
 let registry = registrationNumber();
+//instance of storage function
+let data = storage();
 
+let listOfNumberPlates;
 
-//add button event listener
-addBtn.addEventListener("click", () => {
-    //get the input
-    registry.setInput(regNumInput.value);
-    //get the list of registration numbers
-    registry.setListOfRegNum(registry.getValidation());
-
-    if (registry.setConvert() === registry.getValidation()) {
-        plateList.innerHTML = "";
-        //create registration number item
-        for (let plateItem of registry.getListOfRegNum()) {
-            const li = document.createElement("li");
-            li.className = "plate flex column justify-center align-center";
-            li.appendChild(document.createTextNode(plateItem));
-            plateList.appendChild(li);
-        }
-        alertMsg("update-msg", "Registration Number Added");
-    } else {
-        alertMsg("error-msg", registry.getValidation());
-    }
-
-    //dropdown event listener
-    dropdown.addEventListener("change", () => {
-        registry.setTown(dropdown.options[dropdown.selectedIndex].value);
-        if(dropdown.options[dropdown.selectedIndex].value === "all") {
-            createNumberPlate(registry.getListOfRegNum());
-        } else {
-            createNumberPlate(registry.getFilterList());
-        }
-        console.log(registry.getFilterList());
-    })
-})
-
-//clear list event listener
-clearBtn.addEventListener("click", () => {
-    plateList.innerHTML = "";
-})
 //create number plate function
 const createNumberPlate = list => {
     plateList.innerHTML = "";
@@ -58,6 +24,7 @@ const createNumberPlate = list => {
         plateList.appendChild(li);
     }
 }
+
 //create alert message function
 const alertMsg = (theClass, theMsg) => {
     const p = document.createElement("p");
@@ -66,8 +33,60 @@ const alertMsg = (theClass, theMsg) => {
     headerContent.appendChild(p);
     setTimeout(() => {
         p.remove();
-    }, 2000)
+    }, 1500)
 }
+
+//add button event listener
+addBtn.addEventListener("click", () => {
+    //get the input
+    registry.setInput(regNumInput.value);
+    //get the list of registration numbers
+    registry.setListOfRegNum(registry.getValidation());
+
+    if (registry.setConvert() === registry.getValidation()) {
+        plateList.innerHTML = "";
+
+        //update registration number list with localStorage list
+        
+        if (data.getData("registrationNumbers") === null) {
+            listOfNumberPlates = registry.getListOfRegNum();
+        } else {
+            if(registry.getListOfRegNum().length === 1 && !data.getData("registrationNumbers").includes(registry.getInput())) {
+                registry.listOfRegNum.unshift(...data.getData("registrationNumbers"));
+            }
+            listOfNumberPlates =registry.getListOfRegNum();           
+        }
+        //create registration number item
+        createNumberPlate(listOfNumberPlates)
+
+        alertMsg("update-msg", "Registration Number Added");
+        //store list to localStorage
+        data.setData("registrationNumbers", listOfNumberPlates)
+    } else {
+        alertMsg("error-msg", registry.getValidation());
+    }
+
+})
+
+listOfNumberPlates = data.getData("registrationNumbers");
+createNumberPlate(listOfNumberPlates);
+//dropdown event listener
+dropdown.addEventListener("change", () => {
+    registry.setTown(dropdown.options[dropdown.selectedIndex].value);
+    if(dropdown.options[dropdown.selectedIndex].value === "all") {
+        createNumberPlate(listOfNumberPlates);
+    } else {
+        createNumberPlate(registry.getFilterList(listOfNumberPlates));
+    }
+    console.log(registry.getFilterList(listOfNumberPlates));
+})
+//clear list event listener
+clearBtn.addEventListener("click", () => {
+    plateList.innerHTML = "";
+    localStorage.clear();
+})
+
+
 
 
 
